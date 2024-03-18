1a2b89e3fb42d84ae8fc5f84fadbf3e092c9f4be
parthiban67 <parthibansolomon67@gmail.com>
2024-03-18 22:12:38 +0530

    create new account step completed

--- src/main/kotlin/console/ConsoleApp.kt
+++ src/main/kotlin/console/ConsoleApp.kt
@@ -27,19 +27,37 @@ class ConsoleApp {
             if (proceed == "n") {
                 return
             }
-            println("Enter the account type: ")
-            for ((i, type) in AccountType.values().withIndex()) {
-                println("${i + 1} ${type.name}")
-            }
-            val accountTypeOption = readln().toInt();
-            val accountType: AccountType? = AccountType.values()
-                .filterIndexed { i, _ -> i == accountTypeOption }.firstOrNull()
-            if (accountType == null) {
-                println("!!! Invalid option !!!")
-            }
-            val account: Account? = accountType?.let { AccountFactory.getAccount(it) };
-            val name = readln()
-            account?.name = name
+
+            var accountTypeStep = false
+            var accountType: AccountType?
+            do {
+                println("Enter the account type: ")
+                for ((i, type) in AccountType.entries.withIndex()) {
+                    println("${i + 1} ${type.name}")
+                }
+                val accountTypeOption = readln().toInt();
+                accountType = AccountType.entries.toTypedArray()
+                    .filterIndexed { i, _ -> (i + 1) == accountTypeOption }.firstOrNull()
+                if (accountType == null) {
+                    println("!!! Invalid option !!!")
+                    continue
+                }
+                accountTypeStep = true
+            }while(!accountTypeStep)
+
+            val account: Account? = accountType?.let { AccountFactory.getAccount(it) }
+            var nameStep = false
+            do {
+                println("Enter customer name: ")
+                readln().let{
+                    if(it.isNotBlank()){
+                        account?.name = it
+                        nameStep = true;
+                    }else{
+                        println("!!! Customer name can't be blank !!!")
+                    }
+                }
+            }while(!nameStep)
 
             account?.let{
                 bank.createAccount(it)
